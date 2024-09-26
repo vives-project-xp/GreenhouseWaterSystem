1db8e099eb46e7563460f43006ebbd9721faba9b
Thomas Oddery <thomas.oddery@student.vives.be>
2024-09-26 15:50:40 +0200

    Forcast

---
+++ Forcast.py
@@ -0,0 +1,57 @@
+import openmeteo_requests
+
+import requests_cache
+import pandas as pd
+from retry_requests import retry
+
+# Setup the Open-Meteo API client with cache and retry on error
+cache_session = requests_cache.CachedSession('.cache', expire_after = 3600)
+retry_session = retry(cache_session, retries = 5, backoff_factor = 0.2)
+openmeteo = openmeteo_requests.Client(session = retry_session)
+
+# Make sure all required weather variables are listed here
+# The order of variables in hourly or daily is important to assign them correctly below
+url = "https://api.open-meteo.com/v1/forecast"
+params = {
+	"latitude": 51.1871631,
+	"longitude": 3.2035464,
+	"hourly": ["temperature_2m", "precipitation_probability", "precipitation", "rain", "showers"],
+	"timezone": "auto",
+	"forecast_days": 3
+}
+responses = openmeteo.weather_api(url, params=params)
+
+# Process first location. Add a for-loop for multiple locations or weather models
+response = responses[0]
+#print(f"Coordinates {response.Latitude()}°N {response.Longitude()}°E")
+#print(f"Elevation {response.Elevation()} m asl")
+#print(f"Timezone {response.Timezone()} {response.TimezoneAbbreviation()}")
+#print(f"Timezone difference to GMT+0 {response.UtcOffsetSeconds()} s")
+
+# Process hourly data. The order of variables needs to be the same as requested.
+hourly = response.Hourly()
+hourly_temperature_2m = hourly.Variables(0).ValuesAsNumpy()
+hourly_precipitation_probability = hourly.Variables(1).ValuesAsNumpy()
+hourly_precipitation = hourly.Variables(2).ValuesAsNumpy()
+hourly_rain = hourly.Variables(3).ValuesAsNumpy()
+hourly_showers = hourly.Variables(4).ValuesAsNumpy()
+
+hourly_data = {"date": pd.date_range(
+	start = pd.to_datetime(hourly.Time(), unit = "s", utc = True),
+	end = pd.to_datetime(hourly.TimeEnd(), unit = "s", utc = True),
+	freq = pd.Timedelta(seconds = hourly.Interval()),
+	inclusive = "left"
+)}
+hourly_data["temperature_2m"] = hourly_temperature_2m
+hourly_data["precipitation_probability"] = hourly_precipitation_probability
+hourly_data["precipitation"] = hourly_precipitation
+hourly_data["rain"] = hourly_rain
+hourly_data["showers"] = hourly_showers
+
+pd.set_option('display.max_rows', None)  # Show all rows
+pd.set_option('display.max_columns', None)  # Show all columns
+pd.set_option('display.width', None)  # Prevent line wrapping
+hourly_dataframe = pd.DataFrame(data = hourly_data)
+# Save the hourly dataframe to a CSV file
+hourly_dataframe.to_csv('hourly_precipitation_data.csv', index=False)
+#print(hourly_dataframe)
