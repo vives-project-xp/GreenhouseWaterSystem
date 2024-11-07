dfc8ca6f8f496efbca95c4b90b171bf180d22136
Bram Degeest <degeestbram@gmail.com>
2024-11-07 09:41:50 +0100

    calibration program EC_sensor

---
+++ ProjectFiles/EC_SensorTest_V01/src/main.cpp
@@ -0,0 +1,30 @@
+#include <Arduino.h>
+#include <DFRobot_EC.h>
+#include <EEPROM.h>
+
+#define EC_PIN A1
+float voltage,ecValue,temperature = 25;
+DFRobot_EC ec;
+
+void setup()
+{
+  Serial.begin(115200);  
+  ec.begin();
+}
+
+void loop()
+{
+    static unsigned long timepoint = millis();
+    if(millis()-timepoint>1000U)  //time interval: 1s
+    {
+      timepoint = millis();
+      voltage = analogRead(EC_PIN)/1024.0*5000;   // read the voltage
+      ecValue =  ec.readEC(voltage,temperature);  // convert voltage to EC with temperature compensation
+      Serial.print("temperature:");
+      Serial.print(temperature,1);
+      Serial.print("^C  EC:");
+      Serial.print(ecValue,2);
+      Serial.println("ms/cm");
+    }
+    ec.calibration(voltage,temperature);          // calibration process by Serail CMD
+}
